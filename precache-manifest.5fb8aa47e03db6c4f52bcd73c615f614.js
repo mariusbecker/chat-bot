6900199ecc4f94496c4ba81da3515beea71f7dd2
mariusbecker <becker.m@hotmail.com>
2022-02-25 15:39:05 +0100

    upload all files

---
+++ precache-manifest.5fb8aa47e03db6c4f52bcd73c615f614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4883d66b2240157207bea2df48b5ff34",
+    "url": "./static/media/menu.4883d66b.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f5717e462f46d10bf276",
+    "url": "./static/js/main.6c27e215.chunk.js"
+  },
+  {
+    "revision": "606939c00d7ed247482f",
+    "url": "./static/js/2.a066d280.chunk.js"
+  },
+  {
+    "revision": "f5717e462f46d10bf276",
+    "url": "./static/css/main.5862aeff.chunk.css"
+  },
+  {
+    "revision": "e81d2ea991513c9176e23fd8a8b99036",
+    "url": "./index.html"
+  }
+];
